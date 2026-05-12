9dceee8bb28d2365fe81bd038fd8cf11b74033ba
jerrylemin <101878608+jerrylemin@users.noreply.github.com>
2026-05-13 00:11:22 +0700

    defer reports refresh

--- Views/Pages/ReportsPage.xaml.cs
+++ Views/Pages/ReportsPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using ProjectTest.ViewModels;
@@ -6,6 +7,8 @@ namespace ProjectTest.Views.Pages;
 
 public sealed partial class ReportsPage : Page
 {
+    private bool _initialLoadQueued;
+
     public ReportsPage()
     {
         ViewModel = new ReportsViewModel(App.Current.Services.ReportingService);
@@ -18,11 +21,17 @@ public sealed partial class ReportsPage : Page
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
         base.OnNavigatedTo(e);
-        RefreshPage();
-    }
 
-    private async void RefreshPage()
-    {
-        await ViewModel.RefreshAsync();
+        if (_initialLoadQueued)
+        {
+            _ = ViewModel.InitializeAsync();
+            return;
+        }
+
+        _initialLoadQueued = true;
+        DispatcherQueue.TryEnqueue(DispatcherQueuePriority.Low, async () =>
+        {
+            await ViewModel.InitializeAsync();
+        });
     }
 }
