e6f7d5d9f596502beb3999680667a9fa108e84bb
jerrylemin <jerryle.minh.3@gmail.com>
2026-05-13 02:13:18 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.ApplicationModel.DynamicDependency;
+using Microsoft.Windows.ApplicationModel.WindowsAppRuntime;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using WinRT;
 
@@ -9,28 +11,28 @@ namespace ProjectTest;
 public static class Program
 {
     private const uint WindowsAppSdkMajorMinorVersion = 0x00010008;
+    private const int AppModelErrorNoPackage = 15700;
 
     [STAThread]
     public static void Main(string[] args)
     {
-        ComWrappersSupport.InitializeComWrappers();
-
         var bootstrapped = false;
 
         try
         {
-            if (!IsRunningPackaged())
+            ComWrappersSupport.InitializeComWrappers();
+
+            if (IsRunningPackaged())
+            {
+                DeploymentManager.Initialize();
+            }
+            else
             {
                 Bootstrap.Initialize(WindowsAppSdkMajorMinorVersion);
                 bootstrapped = true;
             }
 
-            Application.Start(_ =>
-            {
-                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
-                SynchronizationContext.SetSynchronizationContext(context);
-                var app = new App();
-            });
+            StartWinUIApplication();
         }
         finally
         {
@@ -41,6 +43,19 @@ public static class Program
         }
     }
 
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void StartWinUIApplication()
+    {
+        Application.Start(_ =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(
+                DispatcherQueue.GetForCurrentThread());
+
+            SynchronizationContext.SetSynchronizationContext(context);
+            var app = new App();
+        });
+    }
+
     private static bool IsRunningPackaged()
     {
         var length = 0;
@@ -48,8 +63,8 @@ public static class Program
         return result != AppModelErrorNoPackage;
     }
 
-    private const int AppModelErrorNoPackage = 15700;
-
     [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
-    private static extern int GetCurrentPackageFullName(ref int packageFullNameLength, string? packageFullName);
+    private static extern int GetCurrentPackageFullName(
+        ref int packageFullNameLength,
+        string? packageFullName);
 }
